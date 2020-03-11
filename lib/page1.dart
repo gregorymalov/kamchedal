e6cd8257bafb41670803934e1284be6aa6563784
Gregory M <gregory.malov@gmail.com>
2020-03-11 14:47:58 +1200

    add background app

--- lib/page1.dart
+++ lib/page1.dart
@@ -24,183 +24,196 @@ class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: GFColors.DARK,
-        appBar: AppBar(
-          title: Text('КамчЕДАл Отзывы'),
-        ),
-        body: Builder(builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
+      backgroundColor: GFColors.DARK,
+      appBar: AppBar(
+        title: Text('КамчЕДАл Отзывы'),
+      ),
+      body: Stack(
+        children: <Widget>[
+      
+            Positioned.fill(
+            child: Image.asset(
+              'assets/bg.png',
+            fit: BoxFit.fill,
+            ),
+          ),
+           Builder(builder: (context) {
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
 //Лого
-                Container(
-                  padding: EdgeInsets.all(50.0),
-                  child: GFImageOverlay(
-                    height: 180,
-                    width: 180,
-                    padding: EdgeInsets.all(20.0),
-                    shape: BoxShape.circle,
-                    image: AssetImage('assets/icon.png'),
+                  Container(
+                    padding: EdgeInsets.all(45.0),
+                    child: GFImageOverlay(
+                      height: 170,
+                      width: 170,
+                      padding: EdgeInsets.all(10.0),
+                      shape: BoxShape.circle,
+                      image: AssetImage('assets/icon.png'),
+                    ),
                   ),
-                ),
 
 //Текст
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    'Пожалуйста оцените данное заведение нажав соответственную звёздочку и ниже оставьте свой отзыв. Мы работаем, над усовершенствованием сервиса обслуживания в нашем крае, ваши отзывы, предложения и замечания не останутся без внимания!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
+                  Container(
+                    padding: EdgeInsets.all(17.0),
+                    child: Text(
+                      'Пожалуйста оцените данное заведение нажав соответственную звёздочку и ниже оставьте свой отзыв. Мы работаем, над усовершенствованием сервиса обслуживания в нашем крае, ваши отзывы, предложения и замечания не останутся без внимания!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
-                ),
 
 //Звёзды
-                Container(
-                  width: 350,
-                  padding: EdgeInsets.all(20.0),
-                  child: GFRating(
-                    value: _rating,
-                    onChanged: (value) {
-                      setState(() {
-                        _rating = value;
-                      });
-                    },
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.all(20.0),
+                    child: GFRating(
+                      value: _rating,
+                      onChanged: (value) {
+                        setState(() {
+                          _rating = value;
+                        });
+                      },
+                    ),
                   ),
-                ),
 
 //Филд Ваше Имя
-                Container(
-                  width: 350,
-                  padding: EdgeInsets.all(10.0),
-                  child: TextField(
-                    autocorrect: true,
-                    onChanged: (text) {
-                      name = text;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Введите Ваше Имя',
-                      prefixIcon: Icon(Icons.person),
-                      hintStyle: TextStyle(color: Colors.grey),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      autocorrect: true,
+                      onChanged: (text) {
+                        name = text;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Введите Ваше Имя',
+                        prefixIcon: Icon(Icons.person),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                //Филд ваш телефон
-                Container(
-                  width: 350,
-                  padding: EdgeInsets.all(10.0),
-                  child: TextField(
-                    autocorrect: true,
-                    onChanged: (text) {
-                      phone = text;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Введите Ваш Телефон',
-                      prefixIcon: Icon(Icons.phone),
-                      hintStyle: TextStyle(color: Colors.grey),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
+                  //Филд ваш телефон
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      autocorrect: true,
+                      onChanged: (text) {
+                        phone = text;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Введите Ваш Телефон',
+                        prefixIcon: Icon(Icons.phone),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
                       ),
                     ),
                   ),
-                ),
 
 //Филд Ваш отзыв
-                Container(
-                  width: 350,
-                  padding: EdgeInsets.all(10.0),
-                  child: TextField(
-                    autocorrect: true,
-                    onChanged: (text) {
-                      review = text;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Ваш Отзыв',
-                      prefixIcon: Icon(Icons.star_border),
-                      hintStyle: TextStyle(color: Colors.grey),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.green, width: 2),
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      autocorrect: true,
+                      onChanged: (text) {
+                        review = text;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Ваш Отзыв',
+                        prefixIcon: Icon(Icons.star_border),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: Colors.green, width: 2),
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                //Кнопка отправить
-                Container(
-                  width: 350,
-                  padding: EdgeInsets.all(10.0),
-                  child: GFButton(
-                    onPressed: () async {
-                      print(review);
-                      // FlutterOpenWhatsapp.sendSingleMessage("9004387824", "Hello");
-                      try {
-                        await Firestore.instance.runTransaction((t) {
-                          return t.set(
-                              Firestore.instance
-                                  //  .document('reviews/$phone|$name'),
-                                  .document('reviews/$time|$name'),
-                              {
-                                'review': review,
-                                'phone': phone,
-                                'name': name,
-                                'rating': _rating,
-                                'Дата': time
-                              });
-                        });
+                  //Кнопка отправить
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.all(10.0),
+                    child: GFButton(
+                      onPressed: () async {
+                        print(review);
+                        // FlutterOpenWhatsapp.sendSingleMessage("9004387824", "Hello");
+                        try {
+                          await Firestore.instance.runTransaction((t) {
+                            return t.set(
+                                Firestore.instance
+                                    //  .document('reviews/$phone|$name'),
+                                    .document('reviews/$time|$name'),
+                                {
+                                  'review': review,
+                                  'phone': phone,
+                                  'name': name,
+                                  'rating': _rating,
+                                  'Дата': time
+                                });
+                          });
 
-                        send();
+                          send();
 
-                        return Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Page2()));
-                      } catch (e) {
-                        print(e);
-                        Scaffold.of(context).showSnackBar(SnackBar(
-                          content: Text('Произошла ошибка $e'),
-                        ));
-                      }
-                    },
+                          return Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Page2()));
+                        } catch (e) {
+                          print(e);
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text('Произошла ошибка $e'),
+                          ));
+                        }
+                      },
 
-                    // Ошибка при переходе на Page2, также при включени�� кода навигатор пуш, пропадает зелёная заливка кнопки
+                      // Ошибка при переходе на Page2, также при включени�� кода навигатор пуш, пропадает зелёная заливка кнопки
 
-                    text: "ОТПРАВИТЬ",
-                    color: Colors.lightGreen,
-                    type: GFButtonType.outline,
-                  ),
-                )
-              ],
-            ),
-          );
-        }));
+                      text: "ОТПРАВИТЬ",
+                      color: Colors.lightGreen,
+                      type: GFButtonType.outline,
+                    ),
+                  )
+                ],
+              ),
+            );
+          }),
+        ],
+      )
+    
+    );
   }
 
   void send() async {
--- lib/page2.dart
+++ lib/page2.dart
@@ -5,48 +5,64 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: GFColors.DARK,
-        appBar: AppBar(
-          title: Text('КамчЕДАл Отзывы'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              GFImageOverlay(
-                height: 180,
-                width: 180,
-                shape: BoxShape.circle,
-                image: AssetImage('assets/icon.png'),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text(
-                  'Наше заведение принимает чаевые через Сбербанк Онлайн! Откройте приложение Сбербанк Онлайн на вашем смартфоне и в разделе платежи выберите \"Оплата через QR\", отсканируйте QR и введите произвольную сумму. ',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: Text.rich(
-                  TextSpan(
-                      text: 'Спасибо ',
-                      children: [WidgetSpan(child: Icon(Icons.favorite, color: Colors.red,))]),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ),
-              Container(
-                width: 320,
-                padding: EdgeInsets.all(10.0),
-                child: Image.asset('assets/qr.png'),
-              ),
-            ],
+      backgroundColor: GFColors.DARK,
+      appBar: AppBar(
+        title: Text('КамчЕДАл Отзывы'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Image.asset(
+              'assets/bg.png',
+              fit: BoxFit.fill,
+            ),
           ),
-        ));
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                GFImageOverlay(
+                  height: 180,
+                  width: 180,
+                  shape: BoxShape.circle,
+                  image: AssetImage('assets/icon.png'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'Наше заведение принимает чаевые через Сбербанк Онлайн! Откройте приложение Сбербанк Онлайн на вашем смартфоне и в разделе платежи выберите \"Оплата через QR\", отсканируйте QR и введите произвольную сумму. ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text.rich(
+                    TextSpan(text: 'Спасибо ', children: [
+                      WidgetSpan(
+                          child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ))
+                    ]),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 320,
+                  padding: EdgeInsets.all(10.0),
+                  child: Image.asset('assets/qr.png'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
